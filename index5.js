d8cbb20fe5f78d6d19367d882eae50eed6cb7081
vetalz <vitaly482@gmail.com>
2022-04-01 14:36:26 +0300

    class queue

--- index5.js
+++ index5.js
@@ -1,52 +1,50 @@
-const allowedChars = ['a', 'b'];
-
 class Queue {
-  constructor(concurrency = 2) {
+  constructor(concurrency = 20) {
     this.concurrency = concurrency;
   }
-  items = []
+
+  tasks = [];
   count = 0;
-  isFound = false;
-  isFree = true;
-  add(item){
-    this.items.push(item);
+
+  add(task) {
+    this.tasks.push(task);
+    this.execute()
   }
-  execute(){
+
+  isFree() {
+    return this.count < this.concurrency;
+  }
+
+  execute() {
     if (this.count < this.concurrency) {
-      this.count++
-      let promise = this.items.shift()
-      promise[0](promise[1]).then((result) => this.fulfilled(result))
-    } else {
-      this.isFree = false;
+      this.count++;
+      const task = this.tasks.shift()
+      const [callback, args] = task;
+      callback(...args).then((result) => this.fulfilled(result, task))
+      console.log('promise:', args[0])
     }
   }
-  fulfilled(result) {
-    console.log('fulfilled')
-    this.count--
-    this.isFree = true
-    if (result) {
-      this.isFound = true
+
+  fulfilled(result, args) {
+    console.log('fulfilled:', args[1][0]);
+    if (!this.isDone()) {
+      return;
     }
+    this.count--;
+    this.execute();
+  }
+
+  isDone() {
+    return this.tasks.length;
   }
 }
 
-const iterator = brute();
 const queue = new Queue();
 
-while (true) {
-  if (queue.isFree) {
-    let itemObj = iterator.next();
-    if (itemObj.done) {
-      break;
-    }
-    queue.add([login, itemObj.value])
-    queue.execute()
-  }
-  if (queue.isFound) {
-    console.log('found');
-    break;
-  }
+for (let i=0; i<200; i++) {
+  queue.add([login, [i]])
 }
+
 
 
 
@@ -64,37 +62,4 @@ async function login(password) {
   const time = getRandomTime(2000);
   await sleep(time);
   return password === 'aa';
-}
-
-function* brute(maxLength=2) {
-  for (let i=1; i<=maxLength; i++) {
-    let rawPassword = createPasswordTemplate(i);
-    while(rawPassword) {
-      yield convertPassword(rawPassword)
-      rawPassword = generatorPassword(rawPassword)
-    }
-  }
-}
-
-function createPasswordTemplate(length) {
-  return Array(length).fill(0);
-}
-
-function convertPassword(rawPass) {
-  let pass = '';
-  for (let i of rawPass) {
-    pass += allowedChars[i];
-  }
-  return pass;
-}
-
-function generatorPassword(rawPass) {
-  for (let i=0; i<rawPass.length; i++) {
-    if (rawPass[i] < allowedChars.length - 1) {
-      rawPass[i] += 1;
-      return rawPass;
-    }
-    rawPass[i] = 0;
-  }
-  return null;
 }
